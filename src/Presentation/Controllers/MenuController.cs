09699778d581059cca08b79df642ce5e03f86bbd
Taher Elmarengoze <87097223+TaherMarengoze@users.noreply.github.com>
2025-08-16 21:42:36 +0300

    Add Test endpoint to `MenuController`

--- src/Presentation/Controllers/MenuController.cs
+++ src/Presentation/Controllers/MenuController.cs
@@ -1,7 +1,15 @@
 ﻿
+using Microsoft.AspNetCore.Mvc;
+
 namespace Presentation.Controllers;
 
 public class MenuController : ApiController
 {
+    [HttpGet]
+    public async Task<ActionResult<string>> TestControllerAsync()
+    {
+        await Task.Delay(100);
 
+        return Ok($"{nameof(MenuController)}: Test Passed");
+    }
 }
