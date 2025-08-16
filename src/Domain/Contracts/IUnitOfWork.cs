826d11f77218dff77410f91700e2b0a6debe028d
Taher Elmarengoze <87097223+TaherMarengoze@users.noreply.github.com>
2025-08-16 12:21:58 +0300

    Add `IUnitOfWork` to `Core` layer inside `Domain` under `Contarcts` and implementation to `Infrastructure` layer in `Persistence` under `Repositories`

---
+++ src/Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿
+using Domain.Models;
+
+namespace Domain.Contracts;
+
+public interface IUnitOfWork
+{
+    public Task<int> CommitAsync();
+
+    IGenericRepository<TModel, TKey> GetRepository<TModel, TKey>()
+        where TModel : BaseModel<TKey>
+        where TKey : struct;
+}
---
+++ src/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿
+using Domain.Contracts;
+using Domain.Models;
+using System.Collections.Concurrent;
+
+namespace Persistence.Repositories;
+
+public class UnitOfWork(RestaurantDbContext context) : IUnitOfWork
+{
+    private ConcurrentDictionary<string, object> repositories;
+
+    public async Task<int> CommitAsync() => await context.SaveChangesAsync();
+
+    public IGenericRepository<TModel, TKey> GetRepository<TModel, TKey>()
+        where TModel : BaseModel<TKey>
+        where TKey : struct
+    {
+        repositories ??= new ConcurrentDictionary<string, object>();
+
+        return (IGenericRepository<TModel, TKey>)repositories
+            .GetOrAdd(
+                key: typeof(TModel).Name,
+                value: _ = new GenericRepository<TModel, TKey>(context)
+            );
+    }
+}
