f87bd7fd610cbdfef80ed25ac63339cc3d2efb23
Taher Elmarengoze <87097223+TaherMarengoze@users.noreply.github.com>
2025-08-16 12:11:50 +0300

    Add `GenericRepository` to `Persistence\Repositories`

---
+++ src/Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,17 @@
+﻿
+using Domain.Contracts;
+using Domain.Models;
+
+namespace Persistence.Repositories;
+
+public class GenericRepository<TModel, TKey>(RestaurantDbContext context)
+    : IGenericRepository<TModel, TKey>
+    where TModel : BaseModel<TKey>
+    where TKey : struct
+{
+    public async Task<TModel> GetAsync(TKey id) => throw new NotImplementedException();
+    public async Task<TModel> GetAllAsync(bool tracked = false) => throw new NotImplementedException();
+    public async Task AddAsync(TModel entity) => throw new NotImplementedException();
+    public void Update(TModel entity) => throw new NotImplementedException();
+    public void Delete(TModel entity) => throw new NotImplementedException();
+}
