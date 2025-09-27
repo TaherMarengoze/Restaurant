586814b349de45a8192f48e37e405f53b59313db
Taher Elmarengoze <87097223+TaherMarengoze@users.noreply.github.com>
2025-09-27 12:36:24 +0300

    Add DbInitializer

---
+++ src/Persistence/DbInitializer.cs
@@ -0,0 +1,22 @@
+﻿
+using Domain.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class DbInitializer(RestaurantDbContext dbContext)
+    : IDbInitializer
+{
+    public async Task InitializeAsync()
+    {
+        await CheckForPendingMigrationsAsync(dbContext);
+    }
+
+    private static async Task CheckForPendingMigrationsAsync(DbContext dbContext)
+    {
+        if (dbContext.Database.GetPendingMigrations().Any())
+        {
+            await dbContext.Database.MigrateAsync();
+        }
+    }
+}
